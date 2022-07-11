44d34a4641936dbbe938d33446b739e2fdf0cefd
thedoys <pathaphi.p@gmail.com>
2022-07-11 15:10:32 +0700

    Get ip

--- src/main/java/com/csproject/homealoneservice/controllers/Controller.java
+++ src/main/java/com/csproject/homealoneservice/controllers/Controller.java
@@ -4,12 +4,53 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(value = "")
 public class Controller {
     @GetMapping(value = "")
-    public String test(){
+    public String test(HttpServletRequest request){
+
+        System.out.println(request.getLocalAddr());
+        System.out.println(request.getRemotePort());
+        System.out.println(request.getRemoteAddr());
+        System.out.println(getClientIpAddressIfServletRequestExist());
         return "Home Alone Web Service";
+    }
+
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
+
+    public static String getClientIpAddressIfServletRequestExist() {
+
+        if (RequestContextHolder.getRequestAttributes() == null) {
+            return "0.0.0.0";
+        }
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        for (String header: IP_HEADER_CANDIDATES) {
+            String ipList = request.getHeader(header);
+            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                String ip = ipList.split(",")[0];
+                return ip;
+            }
+        }
+
+        return request.getRemoteAddr();
     }
 }
